39a9f7ce0d6a2e7289d76adc93ca94da539316a3
tringapps-sarankumar <123533909+tringapps-sarankumar@users.noreply.github.com>
2023-03-09 15:30:08 +0530

    initial commit

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const setSuccess = element => {
 };
 
 const isValidEmail = email => {
-    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 
